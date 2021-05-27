15736430626810b7ebb5a17eec0480b67cb6de63
Mateusz Sieniawski <msieniawski98@gmail.com>
2021-05-27 15:48:49 +0200

    Lab7 still.

--- lab7/mpi-lab-01/bench/benchmark_v3.c
+++ lab7/mpi-lab-01/bench/benchmark_v3.c
@@ -11,44 +11,56 @@
 
 int main(int argc, char * argv[])
 {
+    MPI_Init(&argc,&argv); /* intialize the library with parameters caught by the runtime */
     
     struct timespec spec;
-
-    MPI_Init(&argc,&argv); /* intialize the library with parameters caught by the runtime */
-   
-    clock_gettime(CLOCK_REALTIME, &spec);
     srand(spec.tv_nsec); // use nsec to have a different value across different processes
 
     int numProcesses, myRank;
-
     MPI_Status *status;
-
     MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+    
+    double startTime;
+    double endTime;
+    double executionTime;
 
-    int buf_send[1] = {1};
-    int buf_recv[1] = {0};
+    int length;
+    int no_experiments = 30;
+    int no_lengths = 7;
+    int lengths[7] = {1, 10, 100, 1000, 10000, 100000, 1000000};
+
+    // Initialize buffers
+    int buf_send[1000000];
+    int buf_recv[1000000];
+    for (int i=0; i < 1000000; i++) {
+        buf_send[i] = 0;
+        buf_recv[i] = 0;
+    }
+
+    for (int i=0; i<no_lengths; i++) {
+	    for (int experiment=0; experiment<no_experiments; experiment++) {
+
+    length = lengths[i];
+
+    // Measure execution time
+    clock_gettime(CLOCK_REALTIME, &spec);
+    startTime = MPI_Wtime();
 
     if (myRank == 0) {
-	printf("Hello world from main process. There are %d", numProcesses);
-	MPI_Send(buf_send, 1, MPI_INT, 1, 13, MPI_COMM_WORLD);
-	MPI_Recv(buf_recv, 1, MPI_INT, numProcesses - 1, 13, MPI_COMM_WORLD, status);
-	printf("Main node. Received %d\n", buf_recv[0]);
-
+	MPI_Send(buf_send, length, MPI_INT, 1, 13, MPI_COMM_WORLD);
+	MPI_Recv(buf_recv, length, MPI_INT, numProcesses - 1, 13, MPI_COMM_WORLD, status);
+	printf("Main node. Received data.\n");
+	endTime = MPI_Wtime();
+	executionTime = endTime - startTime;
+	printf("%d %d %.9lf\n", experiment, length, executionTime);
     }
     else {
-	printf("Hello world from %d sub process.", myRank);
-	MPI_Recv(buf_recv, 1, MPI_INT, myRank - 1, 13, MPI_COMM_WORLD, status);
-	buf_send[0] = buf_recv[0] + myRank;
-	printf("Subprocess %d, received %d, sending %d to %d\n", myRank, buf_recv[0], buf_send[0], (myRank + 1)%numProcesses);
-	MPI_Send(buf_send, 1, MPI_INT, (myRank + 1)%numProcesses, 13, MPI_COMM_WORLD);
+	MPI_Recv(buf_recv, length, MPI_INT, myRank - 1, 13, MPI_COMM_WORLD, status);
+	MPI_Send(buf_send, length, MPI_INT, (myRank + 1)%numProcesses, 13, MPI_COMM_WORLD);
     }
-
-    
-
-    //unsigned t = rand() % 5;
-    //sleep(t);
-    //printf("Hello world from %d/%d (slept %u s)!\n", 0, 1, t);
+}
+}
 
     MPI_Finalize(); /* mark that we've finished communicating */
     
